56d959dc7b14d32c0d58d55d13447c89e0faad2f
Alwin <alwinjohn231@gmail.com>
2024-11-03 00:41:03 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'mamba':'#202124',
+        'useless-gray':'#3B4455',
+        'primary-color':'#92B4F3',
+      }
+    },
+  },  
+  plugins: [],
+}
+
